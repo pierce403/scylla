47781491ad45fdd6d3f8909b83814dec5859837d
pierce403 <pierce403@gmail.com>
2020-01-22 00:22:44 -0800

    A tiny dash of incentivization..
    This change slowly increases the deposit amounts and withdrawal amounts over time (by count) such that users are incentivized to leave their ETH in the mixer as long as possible. This should, in theory, legitimize the mixer as a DeFi project, allowing liquidity providers to collect usage fees from regular users, while at the same time vastly improving the anonymity set.

--- scylla.sol
+++ scylla.sol
@@ -34,6 +34,9 @@ contract MerkleTreeWithHistory {
   uint32 public constant ROOT_HISTORY_SIZE = 100;
   bytes32[ROOT_HISTORY_SIZE] public roots;
 
+  uint public depositCount = 0;
+  uint public withdrawlCount = 0;
+
   constructor(uint32 _treeLevels) public {
     require(_treeLevels > 0, "_treeLevels should be greater than zero");
     require(_treeLevels < 32, "_treeLevels should be less than 32");
@@ -316,7 +319,8 @@ contract ETHTornado is Tornado {
   }
 
   function _processDeposit() internal {
-    require(msg.value == denomination, "Please send `mixDenomination` ETH along with transaction");
+    require(msg.value == denomination + (depositCount*denomination/1000), "Please send `mixDenomination` ETH along with transaction");
+    depositCount = depositCount + 1;
   }
 
   function _processWithdraw(address payable _recipient, address payable _relayer, uint256 _fee, uint256 _refund) internal {
@@ -324,8 +328,9 @@ contract ETHTornado is Tornado {
     require(msg.value == 0, "Message value is supposed to be zero for ETH instance");
     require(_refund == 0, "Refund value is supposed to be zero for ETH instance");
 
-    (bool success, ) = _recipient.call.value(denomination - _fee)("");
+    (bool success, ) = _recipient.call.value(denomination + (withdrawlCount*denomination/1000) - _fee)("");
     require(success, "payment to _recipient did not go thru");
+    withdrawlCount = withdrawlCount + 1;
     if (_fee > 0) {
       (success, ) = _relayer.call.value(_fee)("");
       require(success, "payment to _relayer did not go thru");
